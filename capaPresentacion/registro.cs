7ae8be0c30fa31113918ef4b492e11c995e1a240
L3dsoN <andelsonprogrammer@gmail.com>
2021-12-03 13:00:41 -0400

    Base system

---
+++ capaPresentacion/registro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace capaPresentacion
+{
+    public partial class registro : Form
+    {
+        public registro()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //----Getting the value
+            string display = this.comboBox1.Text;
+            MessageBox.Show("Values = " + display);
+        }
+    }
+}
